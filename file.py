2760c111038be0939f153f9fc6c9d6c82c49fbe7
Flossyy <sawczukkarol9@gmail.com>
2021-03-21 22:16:03 +0100

    odd even improvement

--- file.py
+++ file.py
@@ -1,8 +1,27 @@
-val=input("What number are you thinking about:")
-try:
-    if int(val) % 2==0:
-        (print("This number is even!")) 
-    else:
-        (print("This number is odd!"))
-except:
-    print("Not a number :/")
+
+def ifInteger(value):
+    if value.strip().isdigit(): return True
+    else: return False
+
+def main():
+    val=input("What number are you thinking about:")
+    if ifInteger(val) :
+        if int(val) % 2==0:
+            (print("This number is even!")) 
+        else:
+            (print("This number is odd!"))
+    else: 
+        print("Not a integer :/")
+    loop=True
+    while(loop):
+        val=input("Have another? If no input 0:")
+        if ifInteger(val) :
+            if int(val) == 0:break
+            if int(val) % 2==0:
+                (print("This number is even!")) 
+            else:
+                (print("This number is odd!"))
+        else: 
+            print("Not a integer :/")
+main()
+            
